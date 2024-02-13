b081c560dfcb28d4de11c4730b192e3e97259953
sakshi paliwal <paliwalsakshi31@gmail.com>
2024-02-13 11:20:33 +0530

    Main page, with redux and context updated

---
+++ src/components/SecondaryContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../utils/appStore'
+import MovieList from './MovieList';
+
+const SecondaryContainer = () => {
+  const movieList = useSelector((store: RootState)=> store.movies?.nowPlayingMovies);
+  return (
+    <div className=' bg-black'>
+      /** Movie list
+      Title and Movie card */
+      <div className=" mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
+      <MovieList title={"Now Playing"} movies={movieList} />
+      <MovieList title={"Popular"} movies={movieList} />
+      <MovieList title={"Series"} movies={movieList} />
+      </div>
+
+    </div>
+  )
+}
+
+export default SecondaryContainer
